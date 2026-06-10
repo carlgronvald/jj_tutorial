b6756e4ec108fec104e6e5bb00a4197f269f6aa9
carl <cfg@copenhagen-energy.com>
2026-06-10 09:57:34 +0200

    Analysis work

--- analysis.py
+++ analysis.py
@@ -1,1 +1,11 @@
-# TODO: WE NEED A LOT OF IMPORTANT ANALYSIS HERE!
+from analysis_helper import _helper
+
+
+def perform_analysis():
+    num1 = 2
+    num2 = 2
+
+    return _helper(num1, num2)
+
+
+print(f"Analysis result: {perform_analysis()}")
